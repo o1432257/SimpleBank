ae647c56714cc3d31e89a988700939a0d67a42e5
Neo <72015227+o1432257@users.noreply.github.com>
2022-11-09 15:07:00 +0800

    feat: 新增 transferApi 與 mock測試

--- api/server.go
+++ api/server.go
@@ -3,6 +3,8 @@ package api
 import (
 	db "SimpleBank/db/sqlc"
 	"github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin/binding"
+	"github.com/go-playground/validator/v10"
 )
 
 type Server struct {
@@ -14,9 +16,15 @@ func NewServer(store db.Store) *Server {
 	server := &Server{store: store}
 	router := gin.Default()
 
+	if v, ok := binding.Validator.Engine().(*validator.Validate); ok {
+		v.RegisterValidation("currency", validCurrency)
+	}
+
 	router.GET("/account/:id", server.getAccount)
 	router.GET("/account", server.listAccount)
 	router.POST("/account", server.createAccount)
+
+	router.POST("/transfer", server.createTransfer)
 
 	server.router = router
 	return server
